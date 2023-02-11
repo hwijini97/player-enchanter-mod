8875f4cd912174264981db8bc4c50a48e9ccfec3
휘진 <hwijini97@kakao.com>
2023-02-12 01:57:03 +0900

    플레이어 인챈터 작업

--- src/main/java/com/penchant/PlayerEnchantMod.java
+++ src/main/java/com/penchant/PlayerEnchantMod.java
@@ -1,9 +1,13 @@
 package com.penchant;
 
+import com.penchant.block.PlayerEnchanterBlock;
 import com.penchant.item.TanzaniteItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -13,12 +17,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PlayerEnchantMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("penchant");
+	public static final String MOD_ID = "penchant";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final TanzaniteItem TANZANITE = new TanzaniteItem(new FabricItemSettings().maxCount(1).rarity(Rarity.EPIC));
+	public static final PlayerEnchanterBlock PLAYER_ENCHANTER = new PlayerEnchanterBlock(FabricBlockSettings.of(Material.STONE).strength(4.0f));
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registries.ITEM, new Identifier("penchant", TanzaniteItem.NAME), TANZANITE);
+		Registry.register(Registries.ITEM, new Identifier(MOD_ID, TanzaniteItem.NAME), TANZANITE);
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(TANZANITE));
+
+		Registry.register(Registries.BLOCK, new Identifier(MOD_ID, PlayerEnchanterBlock.NAME), PLAYER_ENCHANTER);
+		Registry.register(Registries.ITEM, new Identifier(MOD_ID, PlayerEnchanterBlock.NAME), new BlockItem(PLAYER_ENCHANTER, new FabricItemSettings().maxCount(1).rarity(Rarity.EPIC)));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> entries.add(PLAYER_ENCHANTER));
 	}
 }
---
+++ src/main/java/com/penchant/block/PlayerEnchanterBlock.java
@@ -0,0 +1,42 @@
+package com.penchant.block;
+
+import com.penchant.PlayerEnchantMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class PlayerEnchanterBlock extends Block {
+    public static final String NAME = "player_enchanter";
+
+    public PlayerEnchanterBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        tooltip.add(Text.translatable("block.penchant.player_enchanter.tooltip").formatted(Formatting.AQUA));
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (world.isClient) {
+            // TODO
+            PlayerEnchantMod.LOGGER.info("test");
+        }
+
+        return ActionResult.SUCCESS;
+    }
+}
