8c3cbedf991b9edb80e28d03b9d960326c4ff8a9
휘진 <hwijini97@kakao.com>
2023-02-11 19:00:30 +0900

    탄자나이트로 수정

--- src/main/java/com/henchant/HumanEnchantMod.java
+++ src/main/java/com/henchant/HumanEnchantMod.java
@@ -1,9 +1,9 @@
 package com.henchant;
 
+import com.henchant.item.TanzaniteItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -13,11 +13,11 @@ import org.slf4j.LoggerFactory;
 
 public class HumanEnchantMod implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("henchant");
-	public static final Item OPAL = new Item(new FabricItemSettings().maxCount(1));
+	public static final TanzaniteItem TANZANITE = new TanzaniteItem(new FabricItemSettings().maxCount(1));
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registries.ITEM, new Identifier("henchant", "opal"), OPAL);
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(OPAL));
+		Registry.register(Registries.ITEM, new Identifier("henchant", TanzaniteItem.NAME), TANZANITE);
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(TANZANITE));
 	}
 }
