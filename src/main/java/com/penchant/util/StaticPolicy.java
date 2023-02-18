6d75c358df2ffc9bedad06d6db5068608ebc2259
휘진 <hwijini97@kakao.com>
2023-02-18 16:00:58 +0900

    오류 수정

--- src/main/java/com/penchant/util/StaticPolicy.java
+++ src/main/java/com/penchant/util/StaticPolicy.java
@@ -2,6 +2,6 @@ package com.penchant.util;
 
 public class StaticPolicy {
     public static final String MOD_ID = "penchant";
-    public static final String PACKET_NAME = "playerEnchanted";
+    public static final String PACKET_NAME = "player_enchanted";
     public static final int LEVEL_COST = 40;
 }
